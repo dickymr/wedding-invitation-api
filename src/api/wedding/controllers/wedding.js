8a856ccee414ae210d35690f4caabd3b06394888
Dicky Muhamad R <dickymr.dev@gmail.com>
2022-04-10 03:12:33 +0700

    update schema

--- src/api/wedding/controllers/wedding.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  wedding controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::wedding.wedding');
--- src/api/wedding/services/wedding.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * wedding service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::wedding.wedding');
